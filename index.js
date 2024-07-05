b363a08272a3febb5fee19a1d659c68f48e86ef4
siwakorn.k <siwakorn.k@blendata.co>
2024-07-05 21:59:54 +0700

    testdeploy2

--- index.js
+++ index.js
@@ -2,7 +2,33 @@
 const express = require('express')
 const app = express()
 const PORT = 4000
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const connectDb = require('./config/dbConnection')
+const dotenv = require('dotenv').config()
+const { notFound, errorHandler } = require('./middlewares/errorHandler')
+const cors = require('cors')
 
+// const port = process.env.PORT || 5050
+app.use(cors())
+//app.use(morgan('dev')).
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser())
+
+app.use('/auth', require('./routes/authRoute'))
+app.use('/users', require('./routes/userRoute'))
+app.use('/products', require('./routes/productRoute'))
+app.use('/carts', require('./routes/cartRoute'))
+app.use('/orders', require('./routes/orderRoute'))
+app.use('/coupons', require('./routes/couponRoute'))
+
+app.use(notFound)
+app.use(errorHandler)
+app.get("/", (req, res) => {
+    res.send("Express on Vercel");
+});
 
 app.get('/home', (req, res) => {
     res.status(200).json('Welcome, your app is working well');

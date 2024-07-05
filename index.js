36102fca4a899ad613abdf95f39ec5becb8726b1
siwakorn.k <siwakorn.k@blendata.co>
2024-07-05 22:12:05 +0700

    testdeploy5

--- index.js
+++ index.js
@@ -11,21 +11,21 @@ const { notFound, errorHandler } = require('./middlewares/errorHandler')
 const cors = require('cors')
 
 // const port = process.env.PORT || 5050
-// app.use(cors())
-// //app.use(morgan('dev')).
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({extended: false}))
-// app.use(cookieParser())
-//
-// app.use('/auth', require('./routes/authRoute'))
-// app.use('/users', require('./routes/userRoute'))
-// app.use('/products', require('./routes/productRoute'))
-// app.use('/carts', require('./routes/cartRoute'))
-// app.use('/orders', require('./routes/orderRoute'))
-// app.use('/coupons', require('./routes/couponRoute'))
-//
-// app.use(notFound)
-// app.use(errorHandler)
+app.use(cors())
+//app.use(morgan('dev')).
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser())
+
+app.use('/auth', require('./routes/authRoute'))
+app.use('/users', require('./routes/userRoute'))
+app.use('/products', require('./routes/productRoute'))
+app.use('/carts', require('./routes/cartRoute'))
+app.use('/orders', require('./routes/orderRoute'))
+app.use('/coupons', require('./routes/couponRoute'))
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.get('/home', (req, res) => {
     res.status(200).json('Welcome, your app is working well');
